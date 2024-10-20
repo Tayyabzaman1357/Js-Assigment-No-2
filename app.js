872236afc2f19fb3f639a614277b950dc248763f
Tayyab Zaman <tayyabzaman807@gmail.com>
2024-10-20 07:21:17 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,85 @@
+                            //   Assigment   2   //
+
+                            // Q1:
+                            //Ans 1:  var userName="";
+
+
+
+
+                            // Q2:
+                          //  Ans 2: 
+                        //var myName="Tayyab Zaman";
+                           //console.log(myName);
+
+
+
+
+                        // Q3:
+                        // Ans 3:
+                        // var message="Hello World";
+                        // alert(message);
+
+
+
+                        // Q4:
+                        // Ans No 4:
+
+
+// Solved By Alert:
+
+ var StudentName= "Name" +  ":" + "Tayyab";
+ alert(StudentName);
+ var studentAge="Age"    +     ":" + 15;
+  alert(studentAge);
+ var studentCenter= "Center"   +    ":" + "Student At SMIT";
+ alert(studentCenter);
+
+
+
+
+
+                        // Ans 4: Solved By Object :
+//                         var string = studentData ={
+// name:"Tayyab",
+ //age:15,
+// center:"Student At SMIT"
+
+//                         }
+               //  console.log(studentData);
+
+
+
+
+
+// Q5:
+// Ans No 5:
+ var fastFood="PIZZA" + "\n" + "PIZZ" + "\n" + "PIZ" + "\n" + "PI" + "\n" + "P";
+ alert(fastFood);
+
+
+
+
+// Q6:
+//  Ans No 6:
+ var email= "My email address is" + " tayyabzaman807@gmail.com";
+ alert(email);
+
+
+
+// Q7:
+// Ans No 7:
+ var book= "A smarter way to learn JavaScript";
+ alert("I am trying to learn from the Book A smarter  way to learn JavaScript ")
+                                                                                                                               
+                        
+// Q8:
+// Ans No 8:
+ document.write("Yeah! I can write HTML content by JavaScript.")
+
+
+
+// Q9:
+// Ans No 9:
+ var design= "  “▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”   ";
+ alert(design);
+                          
